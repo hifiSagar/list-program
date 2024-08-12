aa14a1b4c1bd4f00c0ddda1f848184c095397a9c
SAGAR T <ssagu6661@gmail.com>
2024-08-12 12:41:08 -0400

    Add files via upload

---
+++ c10.py
@@ -0,0 +1,16 @@
+
+#lists
+
+name= input("what is your name ")
+print("HELLO",name)
+
+age=int(input("How Old Are You "))
+print("AUTOMOBILE PARTS")
+name2 = list["Brake", "speedometer cable","mirror"]
+print(name2)
+
+print("simple list ")
+name3 = ["1 ","5","10"]
+print(name3)
+name3 [1]=2
+print(name3)
